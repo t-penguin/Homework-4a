6b11bfbf899c557ec5cfb3df9a3b3e6639604b01
Joseph Collado <sourpina@gmail.com>
2025-02-26 18:18:50 -0500

    Added fields and properties

--- Homework4/Card.cs
+++ Homework4/Card.cs
@@ -2,14 +2,18 @@
 {
     //Fields, example: Rank rank;
     //check the help documentation for the fields
-
+    private Rank _rank;
+    private Suit _suit;
+    private bool _faceUp;
 
     //Card Constructor
-
+    
 
     //Define properties for all above fields
     //code example: public Suit Suit { get { return suit; } }
-
+    public Rank Rank { get { return _rank; } }
+    public Suit Suit { get { return _suit; } }
+    public bool FaceUp { get { return _faceUp; } }
 
     public void FlipOver()
     {
