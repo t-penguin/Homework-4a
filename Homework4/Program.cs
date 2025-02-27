033f548ceb0407050d0066ac18792aab76911b01
Joseph Collado <sourpina@gmail.com>
2025-02-27 11:57:54 -0500

    Added test for Deck cutting

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -1,3 +1,4 @@
+// Test creation and flipping of a singular card
 Card card = new Card(Rank.Ten, Suit.Diamonds);
 Console.WriteLine($"Created card {card.Rank} of {card.Suit}. Face Up: {card.FaceUp}");
 card.FlipOver();
@@ -5,16 +6,28 @@ Console.WriteLine($"Flipped card over. Face Up: {card.FaceUp}");
 
 Console.WriteLine();
 
+// Test creationg of a deck
 Deck deck = new Deck();
 Console.WriteLine("Created new deck:");
 PrintDeck(deck);
 
 Console.WriteLine();
 
+// Test cutting of a deck
+int cutPosition = 25;
+deck.Cut(cutPosition);
+Console.WriteLine($"Cut the deck at index {cutPosition}");
+PrintDeck(deck);
+
+Console.WriteLine();
+
+// Test shuffling of a deck
 deck.Shuffle();
 Console.WriteLine("Shuffled the deck:");
 PrintDeck(deck);
 
+
+// Prints the deck from top to bottom
 void PrintDeck(Deck d)
 {
     foreach (Card c in d.Cards)
